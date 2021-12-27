79f8e4d18ae2ba48f59726007224b2b942a2589f
hoshimiyaadrian <adriandh460@gmail.com>
2021-12-27 19:16:32 +0700

    delta2 module done

--- delta2_module.v
+++ delta2_module.v
@@ -8,7 +8,7 @@ module delta2_module (
     input [3:0] step, controller;
     input signed [15:0] w3i1, w3i2, w3i3, w3i4;
     input signed [15:0] delta31, delta32, delta33, delta34, dadz;
-    output signed delta2;
+    output signed [15:0] delta2;
 
     reg signed [47:0] temp;
     wire signed [47:0] mul1, mul2, mul3, mul4;
@@ -44,27 +44,46 @@ module delta2_module_tb ();
     reg [3:0] step, controller;
     reg signed [15:0] w3i1, w3i2, w3i3, w3i4;
     reg signed [15:0] delta31, delta32, delta33, delta34, dadz;
-    wire signed delta2;
+    wire signed [15:0] delta2;
 
     delta2_module delta_tb(
-    clk, rst, controller, step, w3i1, w3i2, w3i3, w3i4,
-    delta31, delta32, delta33, delta34, dadz, delta2
-);
+        clk, rst, controller, step, w3i1, w3i2, w3i3, w3i4,
+        delta31, delta32, delta33, delta34, dadz, delta2
+    );
     initial begin 
         clk = 1'b1;
         forever #50 clk = ~clk;
     end
 
     initial begin
+        rst = 1;
         step = 4'd0;
         controller = 4'd4;
-        while (1) begin
-            controller = controller + 4'd1;
-            if (controller == 4'd9) begin
-                controller = 4'd4;
-                step = step + 4'd1;
-            end
-        end
+        #100
+        rst = 0;
+        controller = 4'd5;
+        #100
+        controller = 4'd6;
+        #100
+        controller = 4'd7;
+        #100
+        controller = 4'd8;
+        #100
+        controller = 4'd9;
+        
+        #100
+        step = 4'd1;
+        controller = 4'd4;
+        #100
+        controller = 4'd5;
+        #100
+        controller = 4'd6;
+        #100
+        controller = 4'd7;
+        #100
+        controller = 4'd8;
+        #100
+        controller = 4'd9;
     end
 
     initial begin
@@ -72,10 +91,10 @@ module delta2_module_tb ();
         w3i2 = 16'b000000_1000000000;
         w3i3 = 16'b000000_0100000000;
         w3i4 = 16'b000000_0010000000;
-        delta31 = 16'b000010_0000000000; 
-        delta32 = 16'b000010_0000000000;
-        delta33 = 16'b000010_0000000000;
-        delta34 = 16'b000010_0000000000;
+        delta31 = 16'b001000_0000000000; 
+        delta32 = 16'b001000_0000000000;
+        delta33 = 16'b001000_0000000000;
+        delta34 = 16'b001000_0000000000;
         dadz = 16'b000000_1000000000;
 
         #400
